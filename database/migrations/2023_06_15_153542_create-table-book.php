519aca220a04a76744a6c7b1da184dd540e97b9c
Juneroy Quinimon <eroyjune@gmail.com>
2023-06-15 23:38:43 +0800

    feat: added name column

--- database/migrations/2023_06_15_153542_create-table-book.php
+++ database/migrations/2023_06_15_153542_create-table-book.php
@@ -15,6 +15,7 @@ class CreateTableBook extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
             $table->timestamps();
         });
     }
