041c55971631cec6d32f14664ac745d6f40b9a1c
Juneroy Quinimon <eroyjune@gmail.com>
2023-06-18 23:49:23 +0800

    feat: fixed issues in StudentController not found

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
