a3789442996dfc4ba08f791fea2c53e09af2611d
Juneroy Quinimon <eroyjune@gmail.com>
2023-06-20 23:23:03 +0800

    feat: added new field middle_name

--- database/migrations/2023_06_20_151709_add-column-for-user.php
+++ database/migrations/2023_06_20_151709_add-column-for-user.php
@@ -17,6 +17,7 @@ class AddColumnForUser extends Migration
             //
             $table->string("first_name");
             $table->string("last_name");
+            $table->string("middle_name");
         });
     }
 
