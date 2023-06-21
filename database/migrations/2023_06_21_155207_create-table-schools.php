7b9edd2a478bec94a3c2ecb911dd76a8190081b8
Juneroy Quinimon <eroyjune@gmail.com>
2023-06-21 23:54:55 +0800

    feat: added new field description

--- database/migrations/2023_06_21_155207_create-table-schools.php
+++ database/migrations/2023_06_21_155207_create-table-schools.php
@@ -16,6 +16,7 @@ class CreateTableSchools extends Migration
         Schema::create('schools', function (Blueprint $table) {
             $table->id();
             $table->string("name");
+            $table->longText("description");
             $table->timestamps();
         });
     }
