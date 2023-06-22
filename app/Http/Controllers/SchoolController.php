82df984af80f0b2368aae2a82fca7f0d8393b06e
Juneroy Quinimon <eroyjune@gmail.com>
2023-06-22 23:43:23 +0800

    feat: save new created school details

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -44,6 +44,7 @@ class SchoolController extends Controller
          $school = new SchoolModel;
          $school->name = $request->name;
          $school->description = $request->description;
+         $school->save();
 
         return response()->json([
             "success" => true,
