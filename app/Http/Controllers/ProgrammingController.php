484c905b8afc8d425f7d52798ea96531223a2d48
Juneroy Quinimon <eroyjune@gmail.com>
2023-06-27 23:27:54 +0800

    feat: added saving action for saving programming

--- app/Http/Controllers/ProgrammingController.php
+++ app/Http/Controllers/ProgrammingController.php
@@ -109,6 +109,7 @@ class ProgrammingController extends Controller
         $programming = ProgrammingModel::find($id);
         $programming->name = $request->name;
         $programming->description = $request->description;
+        $programming->save();
     }
 
     /**
