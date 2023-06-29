22e66d91722351ed73e6d1ccb793b81b352a54eb
Juneroy Quinimon <eroyjune@gmail.com>
2023-06-29 23:48:55 +0800

    feat: added message params for unsuccessful update

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -102,7 +102,7 @@ class MusicController extends Controller
         return response()->json([
             "success" => false,
             "data" => false,
-            
+            "message" => ""
         ]);
         
     }
