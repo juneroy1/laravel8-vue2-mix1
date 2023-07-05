a259e6bf64453aa920dfb5d4869891f1e37e74c7
Juneroy Quinimon <eroyjune@gmail.com>
2023-07-05 23:51:14 +0800

    feat: added decent message params in error response

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -57,7 +57,7 @@ class MovieController extends Controller
         return response()->json([
             "success" => false,
             "data" => false,
-            "message" => "",
+            "message" => "Something went wrong in creating movie",
         ]);
         
     }
