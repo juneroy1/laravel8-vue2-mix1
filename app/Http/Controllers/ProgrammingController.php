9976abdf5185893f9d83e16c71245b7035f57ec9
Juneroy Quinimon <eroyjune@gmail.com>
2023-07-08 23:53:49 +0800

    feat: added message params in success response

--- app/Http/Controllers/ProgrammingController.php
+++ app/Http/Controllers/ProgrammingController.php
@@ -75,6 +75,7 @@ class ProgrammingController extends Controller
             return response()->json([
                 "success" => true,
                 "data" => $programming,
+                "message" => ""
             ]);
         }
     }
