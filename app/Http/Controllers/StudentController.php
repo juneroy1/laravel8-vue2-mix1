411c5a0dc2f78276332c5bbf49cb78a6c513312c
Juneroy Quinimon <eroyjune@gmail.com>
2023-07-10 23:52:20 +0800

    feat: added decent message in success response saving student

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -52,7 +52,7 @@ class StudentController extends Controller
             return response()->json([
                 'success' => true,
                 'data' => $student,
-                'message' => '',
+                'message' => 'successfully created new student in the app',
                 
             ]);
         }
