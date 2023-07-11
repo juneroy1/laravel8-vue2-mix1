e51033bcdda4ba826f8a7350ac609e1df75595bf
Juneroy Quinimon <eroyjune@gmail.com>
2023-07-11 23:58:51 +0800

    feat: added decent message in success response

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -82,7 +82,7 @@ class StudentController extends Controller
             return response()->json([
                 'success' => true,
                 'data' => $student,
-                'message' => ""
+                'message' => "Successfully get student details"
             ]);
         }
     }
