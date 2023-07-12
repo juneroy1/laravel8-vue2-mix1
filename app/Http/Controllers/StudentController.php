2266641d3c7a0a848d97a75571ff0e42d4ebf8b8
Juneroy Quinimon <eroyjune@gmail.com>
2023-07-12 23:08:05 +0800

    feat: added student variable in if condition

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -104,7 +104,7 @@ class StudentController extends Controller
         //
         $student = User::find($id);
 
-        if (condition) {
+        if ($student) {
             # code...
         }
     }
