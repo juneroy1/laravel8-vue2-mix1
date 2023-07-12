3fec4b2894738852471a9d2dbd7bd86f8ae783cc
Juneroy Quinimon <eroyjune@gmail.com>
2023-07-12 23:05:56 +0800

    feat: added message params in error message

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -89,6 +89,7 @@ class StudentController extends Controller
         return response()->json([
                 'success' => false,
                 'data' => false,
+                'message' => ""
             ]);
     }
 
