78a7f762929e111c2a5505a4190e32ecc343076c
Juneroy Quinimon <eroyjune@gmail.com>
2023-07-13 23:43:55 +0800

    feat: added store variable in if condition

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -131,7 +131,7 @@ class StudentController extends Controller
         //
         $student = User::find($id);
 
-        if (condition) {
+        if ($student) {
             # code...
         }
     }
