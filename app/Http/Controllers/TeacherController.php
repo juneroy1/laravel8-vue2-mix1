3e7aee84f918564af110aa363ec1d8f0644e7b6b
Juneroy Quinimon <eroyjune@gmail.com>
2023-07-16 23:22:55 +0800

    feat: added decent message in error response show func

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -83,7 +83,7 @@ class TeacherController extends Controller
         return response()->json([
             'success' => false,
             'data' => false,
-            'message'=>""
+            'message'=>"Something went wrong"
         ]);
     }
 
